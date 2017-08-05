3815e6e8a2a9796a67c339df26775c28850b552b
zeeshanhooda <30415208+zeeshanhooda@users.noreply.github.com>
2017-08-04 20:46:08 -0600

    initial commit
    entire app, node modules included in package.json

---
+++ index.js
@@ -0,0 +1,30 @@
+//
+// (c) Redman Technologies 2017
+// Author: Zeeshan Hooda
+// Sources: Socket.io, Node.js, Express.js
+//
+// index.js gateway/entrypoint
+//
+//
+//
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var ngrok = require('ngrok');
+
+var port = 3000;
+var startServerFunction = function(){console.log('listening on port ' + port)};
+
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+  	console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, startServerFunction);
---
+++ index2.js
@@ -0,0 +1,9 @@
+//
+// (c) Redman Technologies 2017
+// Author: Zeeshan Hooda
+// Sources: Socket.io, Node.js, Express.js
+//
+// index2.js is empty
+//
+//
+//
