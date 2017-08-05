5fbdabdd4ab895cdcda4f1277e305f82addd5176
zeeshanhooda <30415208+zeeshanhooda@users.noreply.github.com>
2017-08-04 21:26:19 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket){
+	console.log('a user connected');
   socket.on('chat message', function(msg){
   	console.log('message: ' + msg);
     io.emit('chat message', msg);
